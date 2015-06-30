29f82d26ab5f2a24f94b0aeb2cfa769a36ad3973
Ryan Huard <rhuard13@gmail.com>
2015-06-30 10:26:26 -0700

    added pdbrc

--- rcsetup.sh
+++ rcsetup.sh
@@ -1,7 +1,9 @@
 rm -f ~/.vimrc
 rm -f ~/.bashrc
 rm -f ~/.zshrc
+rm -f ~/.pdbrc
 
 ln -s ~/Documents/RCs/Bash/bashrc ~/.bashrc
 ln -s ~/Documents/RCs/Vim/vimrc ~/.vimrc
 ln -s ~/Documents/RCs/Zsh/zshrc ~/.zshrc
+ln -s ~/Documents/RCs/Pdb/pdbrc ~/.pdbrc
