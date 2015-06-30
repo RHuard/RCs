92d7e503b527d42a6c61e3ed2d16b803571ff92f
Ryan Huard <rhuard13@gmail.com>
2015-06-30 11:15:52 -0700

    adding terminator config

--- rcsetup.sh
+++ rcsetup.sh
@@ -1,9 +1,26 @@
+#function to check it directory exists and if it 
+#does not make it
+function make_dir(){
+    if [ ! -d $1 ]; then
+        mkdir $1
+    fi
+}
+
+#set up rcs
+#delete old ones
 rm -f ~/.vimrc
 rm -f ~/.bashrc
 rm -f ~/.zshrc
 rm -f ~/.pdbrc
 
+#link in new ones
 ln -s ~/Documents/RCs/Bash/bashrc ~/.bashrc
 ln -s ~/Documents/RCs/Vim/vimrc ~/.vimrc
 ln -s ~/Documents/RCs/Zsh/zshrc ~/.zshrc
 ln -s ~/Documents/RCs/Pdb/pdbrc ~/.pdbrc
+
+#set up config files
+#terminator
+mk_dir ~/.config
+mk_dir ~/.config/terminator
+ln -s ./config/terminator/config ~/.config/terminator/config
