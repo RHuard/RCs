1e74b4a8181a17ecd022356260e3e23c6cc55037
Ryan Huard <rhuard13@gmail.com>
2015-07-07 15:38:43 -0700

    xmodmap switch for capslock and excape

--- rcsetup.sh
+++ rcsetup.sh
@@ -12,15 +12,19 @@ rm -f ~/.vimrc
 rm -f ~/.bashrc
 rm -f ~/.zshrc
 rm -f ~/.pdbrc
+rm -f ~/.xinitrc
 
 #link in new ones
 ln -s ~/Documents/RCs/Bash/bashrc ~/.bashrc
 ln -s ~/Documents/RCs/Vim/vimrc ~/.vimrc
 ln -s ~/Documents/RCs/Zsh/zshrc ~/.zshrc
 ln -s ~/Documents/RCs/Pdb/pdbrc ~/.pdbrc
+ln -s ~/Documents/RCs/Xinit/xinit ~/.xinitrc
 
 #set up config files
 #terminator
 mk_dir ~/.config
 mk_dir ~/.config/terminator
 ln -s ./config/terminator/config ~/.config/terminator/config
+#xmodmap for xinitrc
+ln -s ./config/xmodmap/xmodmap ~/.xmodmap
