5cdd113e6f61d7c0e24f3bf85aa7803d181a2f98
Ryan Huard <rhuard13@gmail.com>
2015-07-29 09:29:55 -0700

    getting syntax highlighting

--- firsttimesetup.sh
+++ firsttimesetup.sh
@@ -1,3 +1,8 @@
+#ZSH plugins
+mkdir ~/.oh-my-zsh/plugins/syntax
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/syntax
+
+#Vim Plugins
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle 
 ./install.sh --clang-completer --omnisharp-completer --gocode-completer
