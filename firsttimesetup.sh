2b7f31439be0036ccdd619b275e8f757ef945e82
Ryan Huard <rhuard13@gmail.com>
2015-07-29 09:42:27 -0700

    added in custom theme

--- firsttimesetup.sh
+++ firsttimesetup.sh
@@ -1,10 +1,13 @@
 #ZSH plugins
+#syntax highlighting
 mkdir ~/.oh-my-zsh/plugins/syntax
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/syntax
+#use custom theme
+ln -s ~/RCs/Zsh/ryfletcharm.zsh-theme ~/.oh-my-zsh/themes/ryfletcherm.zsh-theme
 
 #Vim Plugins
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle 
-./install.sh --clang-completer --omnisharp-completer --gocode-completer
-./rcsetup.sh
+~/.vim/bundle/YouCompleteMe/install.sh --clang-completer --omnisharp-completer --gocode-completer
+~/RCs/rcsetup.sh
 vim +PluginInstall +qall
