ad81e1cd2264df46179d7df0dcf572df90d03dd3
Ryan Huard <rhuard13@gmail.com>
2016-10-06 15:15:53 -0700

    Update rcsetup.sh

--- rcsetup.sh
+++ rcsetup.sh
@@ -22,7 +22,7 @@ ln -s $HOME/RCs/Vim/vimrc ~/.vimrc
 ln -s $HOME/RCs/Zsh/zshrc ~/.zshrc
 ln -s $HOME/RCs/Pdb/pdbrc ~/.pdbrc
 mkdir -p ~/.vim/colors
-ln -s $HOME/RCs/Vim/ryslate ~/.vim/colors/ryslate.vim
+ln -s $HOME/RCs/Vim/ryslate.vim ~/.vim/colors/ryslate.vim
 
 #set up config files
 #terminator
