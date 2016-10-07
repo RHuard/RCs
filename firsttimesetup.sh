2fa6e73cecf477e0dd09b69ca4dd277c6c6cc3db
Ryan Huard <rhuard13@gmail.com>
2016-10-07 09:25:31 -0700

    make_dir -> mkdr -p and ~ -> $HOME

--- firsttimesetup.sh
+++ firsttimesetup.sh
@@ -1,21 +1,12 @@
 #! /bin/bash
 
-#function to check it directory exists and if it
-#does not make it
-#TODO update to makdir -p
-function make_dir(){
-    if [ ! -d $1 ]; then
-        mkdir $1
-    fi
-}
-
 #ZSH plugins
 #syntax highlighting
-make_dir ~/.oh-my-zsh/plugins/syntax
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/syntax
+mkdir -p $HOME/.oh-my-zsh/plugins/syntax
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/plugins/syntax
 #use custom theme
-ln -s ~/RCs/Zsh/ryfletcherm.zsh-theme ~/.oh-my-zsh/themes/ryfletcherm.zsh-theme
+ln -s $HOME/RCs/Zsh/ryfletcherm.zsh-theme $HOME/.oh-my-zsh/themes/ryfletcherm.zsh-theme
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 
-~/RCs/rcsetup.sh
+$HOME/RCs/rcsetup.sh
