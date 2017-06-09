ae3b7a99c8d12b581b3379ed0ab6bc2aeaeb459e
rhuard <ryan.huard@isilon.com>
2017-06-09 12:58:41 -0700

    Fix bash prompt make readline into vi mode

--- bin/rcsetup.sh
+++ bin/rcsetup.sh
@@ -16,7 +16,7 @@ rm -f $HOME/.tmux.conf
 rm -f $HOME/.xinitrc
 rm -f $HOME/.xmodmap
 rm -f $HOME/.gitignore_global
-
+rm -f $HOME/.inputrc
 
 #link in new ones
 ln -s $HOME/RCs/Bash/bashrc $HOME/.bashrc
@@ -28,12 +28,13 @@ ln -s $HOME/RCs/Tmux/tmux.conf $HOME/.tmux.conf
 ln -s $HOME/RCs/X/xinitrc $HOME/.xinitrc
 ln -s $HOME/RCs/X/xmodmap $HOME/.xmodmap
 ln -s $HOME/RCs/Git/gitignore_global $HOME/gitignore-global
+ln -s $HOME/RCs/Input/inputrc $HOME/.inputrc
 
 mkdir -p $HOME/.config
 mkdir -p $HOME/.config/terminator
 mkdir -p $HOME/.cinfig/i3
 mkdir -p $HOME/.vim/colors
-mkdir -- $HOME/.vim/syntax
+mkdir -p $HOME/.vim/syntax
 
 ln -s $HOME/RCs/config/terminator/config $HOME/.config/terminator/config
 ln -s $HOME/RCs/config/i3/config $HOME/.config/i3/config
