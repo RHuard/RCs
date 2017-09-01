8c334fa56405c117fa465d7e0485604a993d6439
rhuard <ryan.huard@isilon.com>
2017-09-01 15:47:10 -0700

    was missing the ipython dir

--- bin/rcsetup.sh
+++ bin/rcsetup.sh
@@ -14,6 +14,12 @@ rm -f $HOME/.gitignore_global
 rm -f $HOME/.inputrc
 rm -f $HOME/.ipython/profile_default/ipython_config.py
 
+mkdir -p $HOME/.config
+mkdir -p $HOME/.config/terminator
+mkdir -p $HOME/.vim/colors
+mkdir -p $HOME/.vim/syntax
+mkdir -p $HOME/.ipython/profile_default
+
 #link in new ones
 ln -s $HOME/RCs/Bash/bashrc $HOME/.bashrc
 ln -s $HOME/RCs/Vim/vimrc $HOME/.vimrc
@@ -24,12 +30,6 @@ ln -s $HOME/RCs/Tmux/tmux.conf $HOME/.tmux.conf
 ln -s $HOME/RCs/Git/gitignore_global $HOME/gitignore-global
 ln -s $HOME/RCs/Input/inputrc $HOME/.inputrc
 ln -s $HOME/RCs/Ipython/ipython_config.py ~/.ipython/profile_default/ipython_config.py
-
-mkdir -p $HOME/.config
-mkdir -p $HOME/.config/terminator
-mkdir -p $HOME/.vim/colors
-mkdir -p $HOME/.vim/syntax
-
 ln -s $HOME/RCs/config/terminator/config $HOME/.config/terminator/config
 ln -s $HOME/RCs/Vim/ryslate.vim $HOME/.vim/colors/ryslate.vim
 ln -s $HOME/RCs/Vim/syntax/c.vim $HOME/.vim/syntax/c.vim
