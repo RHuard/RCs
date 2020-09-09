15ff2e9b09a4f644f2abdcddc60c8adbab1ceb09
rhuard <rmhuard@gmail.com>
2020-09-09 10:32:58 +0000

    update firstime setup to setup zsh plugins

--- bin/firsttimesetup.sh
+++ bin/firsttimesetup.sh
@@ -3,7 +3,11 @@
 #ZSH plugins
 #syntax highlighting
 mkdir -p $HOME/.oh-my-zsh/plugins/syntax
+mkdir -p $HOME/.oh-my-zsh/plugins/zsh-autosuggestions
+mkdir -p $HOME/.oh-my-zsh/plugins/alias-tips
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/plugins/syntax
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/plugins/zsh-autosuggestions
+git clone https://github.com/djui/alias-tips $HOME/.oh-my-zsh/plugins/alias-tips
 #use custom theme
 ln -s $HOME/RCs/Zsh/ryfletcherm.zsh-theme $HOME/.oh-my-zsh/themes/ryfletcherm.zsh-theme
 
