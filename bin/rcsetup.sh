2b824c95783482a1a6e15a4106472a1f92f94f57
rhuard <rhuard13@gmail.com>
2020-09-10 23:58:52 -0700

    fix ordering issues

--- bin/rcsetup.sh
+++ bin/rcsetup.sh
@@ -21,7 +21,16 @@ rm -f $HOME/.gitignore_global
 rm -f $HOME/.inputrc
 rm -f $HOME/.ipython/profile_default/ipython_config.py
 
-#link in new ones
+# create dirs if don't exist
+mkdir -p $HOME/.config
+mkdir -p $HOME/.config/terminator
+mkdir -p $HOME/.config/i3
+mkdir -p $HOME/.config/nvim
+mkdir -p $HOME/.config/nvim/colors
+mkdir -p $HOME/.vim/colors
+mkdir -p $HOME/.vim/syntax
+
+# link new rcs
 ln -s $HOME/RCs/Bash/bashrc $HOME/.bashrc
 ln -s $HOME/RCs/Vim/vimrc $HOME/.vimrc
 ln -s $HOME/RCs/Vim/vimrc $HOME/.config/nvim/init.vim
@@ -34,13 +43,6 @@ ln -s $HOME/RCs/X/xmodmap $HOME/.xmodmap
 ln -s $HOME/RCs/Git/gitignore_global $HOME/gitignore-global
 ln -s $HOME/RCs/Input/inputrc $HOME/.inputrc
 ln -s $HOME/RCs/Ipython/ipython_config.py ~/.ipython/profile_default/ipython_config.py
-
-mkdir -p $HOME/.config
-mkdir -p $HOME/.config/terminator
-mkdir -p $HOME/.config/i3
-mkdir -p $HOME/.vim/colors
-mkdir -p $HOME/.vim/syntax
-
 ln -s $HOME/RCs/config/terminator/config $HOME/.config/terminator/config
 ln -s $HOME/RCs/config/i3/config $HOME/.config/i3/config
 ln -s $HOME/RCs/Vim/ryslate.vim $HOME/.vim/colors/ryslate.vim
